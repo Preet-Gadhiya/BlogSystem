04b07c16f91e258752cb3d3bc5e9a2d5f8ba1ce6
Rutvik Patel <patelrutvik.prakash007@gmail.com>
2019-04-06 15:43:18 +0530

    add blog and show complete

---
+++ BlogSystemDotNetClient/BlogSystemDotNetClient/Controllers/BlogController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BlogSystemDotNetClient.ServiceReference1;
+
+namespace BlogSystemDotNetClient.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult AddBlog()
+        {
+            return View();
+        }
+
+        public ActionResult Blog(string title , string content , string blogby , string password)
+        {
+            BlogClient service = new BlogClient();            
+            MyBlog stu = new MyBlog();
+            stu.blogby = blogby;
+            stu.content = content;
+            stu.title = title;
+            stu.password = password;
+            string p=service.AddBlog(stu);
+            return View();
+        }
+        public ActionResult AllBlog()
+        {
+            BlogClient service = new BlogClient();
+            MyBlog[] arr = service.GetAllBlog();
+            return View(arr);
+        }
+    }
+}
